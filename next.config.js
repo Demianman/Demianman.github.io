a13736e9aa21a8882564337da04cf049eb0a927d
吴宜蔓 <wuyiman@wuyimandeMacBook-Pro.local>
2024-10-22 16:36:28 -0700

    更新网站内容和配置文件

--- next.config.js
+++ next.config.js
@@ -11,11 +11,12 @@
 //   };
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  }
-  
-  module.exports = nextConfig
+   output: 'export',
+   reactStrictMode: true,
+   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+ }
+ 
+ module.exports = nextConfig
   
   
